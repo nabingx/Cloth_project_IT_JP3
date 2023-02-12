30295b1abfb5837d6b7a9ec33d0037ae2e308770
thienhustk64 <ducthien1005@gmail.com>
2023-02-12 14:57:57 +0000

    init seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cloth.create( [{title: "test1", number_of_cloths: "1"},
+                {title: "test2", number_of_cloths: "1"}])
+p "123"
